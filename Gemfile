4cde1af167b94eabb497cf67b216bf457393bcf1
axyzdong <axyzdong@gmail.com>
2022-04-09 19:24:03 +0800

    the fifth commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem 'dev-portfolio-blog', '~> 0.1.9'
+gem "jekyll-seo-tag"
+gem "jekyll-feed"
+gem "jekyll-sitemap"
+gem "jekyll-paginate-v2"
+gem "jekyll-minifier"
+gem "jekyll-assets"
+gem "sprockets", "~> 3.7"
+gem "autoprefixer-rails"
+gem "jemoji"
+
+#gemspec
+
---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+function onLoad() {
+  var themeSelector = document.getElementById('themeSelector');
+  for (var themeName in themeMap) {
+    var opt = document.createElement('option');
+    opt.value = themeName;
+    opt.innerHTML = capitalizeFirstLetter(themeName);
+    themeSelector.appendChild(opt);
+  }
+  if (localStorage.getItem('theme') != null) {
+    themeSelector.value = localStorage.getItem('theme');
+    toggleTheme();
+  }
+}
+
+function toggleTheme() {
+  var themeName = themeSelector.value;
+  localStorage.setItem('theme', themeName);
+  var element = document.getElementsByTagName('html')[0];
+  changeTheme(element, themeMap[themeName]);
+}
+
+function changeTheme(element, theme) {
+  element.style.setProperty("--primary-background-color", theme['background-color']);
+  element.style.setProperty("--primary-text-color", theme['text-color']);
+  element.style.setProperty("--primary-highlight-color", theme['highlight-color']);
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
